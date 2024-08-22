ef834cc16e010e645c7b0a1e5106637119f14930
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-08-22 20:39:06 +0530

    Add files via upload

--- CSE224/first.kt
+++ CSE224/first.kt
@@ -1,64 +1,107 @@
-fun main(args: Array<String>) {
-    // print("Hii Friends")
-    // println("Hello guys!!")
+// fun main(args: Array<String>) {
+//     // print("Hii Friends")
+//     // println("Hello guys!!")
 
-    // var a = 40.9
-    // // static binding
-    // // a = 40.5
-    // println(a)
-    // // var try = 34
-    // // println(try)
-    // // var `try` = 34
-    // // println(`try`)
+//     // var a = 40.9
+//     // // static binding
+//     // // a = 40.5
+//     // println(a)
+//     // // var try = 34
+//     // // println(try)
+//     // // var `try` = 34
+//     // // println(`try`)
     
 
-    // ------------- 20 aug---------------
-    // val b = 232
-    // // println(a + b)
-    // var c:Int
-    // // var c:Integer  // won't work
-    // c = a + b
-    // println("-------"+c+"-----------")
+//     // ------------- 20 aug---------------
+//     // val b = 232
+//     // // println(a + b)
+//     // var c:Int
+//     // // var c:Integer  // won't work
+//     // c = a + b
+//     // println("-------"+c+"-----------")
 
     
-    // var __num1 = 2
-    // var num2 = 3
-    // var $num = 32     // not allowed
-    // var 2num =23      // not allowed
-    // var num  2 = 3    // void space is not allowed in the name of the variable
-    // var String=123   // it'll work fine but not recommended
-    // var true = 23    // won't work
-    // var null = 32    // won't work
-    // println((num1>num2)?num1:num2)
+//     // var __num1 = 2
+//     // var num2 = 3
+//     // var $num = 32     // not allowed
+//     // var 2num =23      // not allowed
+//     // var num  2 = 3    // void space is not allowed in the name of the variable
+//     // var String=123   // it'll work fine but not recommended
+//     // var true = 23    // won't work
+//     // var null = 32    // won't work
+//     // println((num1>num2)?num1:num2)
 
 
-    var age:Int = 23
-    println(age)
+//     var age:Int = 23
+//     println(age)
 
-    val person = Person()
-    person.displayInfo()
+//     val person = Person()
+//     person.displayInfo()
+
+
+// }
+
+// fun greet(){
+//     val message: String = "Hello, KOtlin!"
+//     println(message)
+// }
+
+// class Person{
+//     var name:String = "John Doe"
+//     val age:Int = 30
+//     val increment = 1;
+//     fun displayInfo(){
+//         println("Name: name, Age: $age, after one year the age will be ${age + increment}")  // var imperoblel
+//     }
+// }
+
+// // this is a single line comment
+
+// /*
+// this is the multi 
+// line
+// comment
+//  */
+
+// ---------- Array-------------22---------------
+fun main(){
+    // val name: String = """ Amartya
+    //                     Kaushik """
+    // println(name)
+
+    // val canVote : Boolean = true
+    // println(canVote)
+
+    // val arr1 = arrayOf(2,23,223,2333)
+    // val arr2 : Array<Int> = arrayOf(4,45,455,4455)
+    // val arr3 = Array(5) {i -> i * 1}
+
+    // for(i in arr3){
+    //     println(i)
+    // }
+    // for(i in arr2){
+    //     println(i)
+    // }
+    // for(i in arr1){
+    //     println(i)
+    // }
+
+    // // Functions of array
+    // val size = arr3.size
+    // println("size of array 3 is $size")
+
+    // if(arr2.isEmpty()){
+    //     println("Array2 is an empty array")
+    // }else{
+    //     println("Array2 is not an empty array")
+    // }
+
+    // val index = arr2.indexOf(45)
+    // println(index)
+
+    // val matrix: Array<A
 
 
 }
-
-fun greet(){
-    val message: String = "Hello, KOtlin!"
-    println(message)
-}
-
-class Person{
-    var name:String = "John Doe"
-    val age:Int = 30
-    val increment = 1;
-    fun displayInfo(){
-        println("Name: name, Age: $age, after one year the age will be ${age + increment}")  // var imperoblel
-    }
-}
-
-// this is a single line comment
-
-/*
-this is the multi 
-line
-comment
- */
+//------------------------HOME WORK---------------------------
+// check all the functions which are available with arrays in kotlin 
