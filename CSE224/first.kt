f95ef826ce127c8254d4ce09014890fc5ea3e89e
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-08-27 15:15:12 +0530

    Add files via upload

--- CSE224/first.kt
+++ CSE224/first.kt
@@ -101,7 +101,153 @@ fun main(){
 
     // val matrix: Array<A
 
+    //-------------26 AUG---------------
+    //------Operators----------
+    var x = 5;
+    println(x.shr(2));
+
+    var y = 23;
+    println(x<y  && y>x);
+    var c =  x and y;
+    println(c);
+    // println(x.and(y));
+    // println(shl(x));
+    
+    //----------------ELVISH OPERATOR---------------
+    var str: String ? = null
+    var str2: String? = "May be declare nullable stirng"
+    var len1: Int = str ?.length ?: -1
+    var len2: Int = str2 ?.length ?: -1
+
+    // println("length of str is ${len1}")
+    // println("length of str2 is ${len2}")
+    // println("-----------While and Do-While------")
+    // var n = 5;
+    // While(n< 10){
+    //     println("----$n-----")
+    //     n++
+    // }do{
+    //     println("------------+++$n+++----------")
+    // }
+
+    //-------------use of WHEN-----------
+    var choice = 3;
+    var result = when(choice){
+        1 -> "One"
+        2 -> "Two"
+        3 -> "Three"
+        else -> "Default value"
+    }
+
+    println(result)
+
+    //-------------Usage of Of----------
+    for(i in 6 until 10){
+        println(i)
+    }
+    println(1.to(10))
+
+    for(i in 16 downTo 10 step 2){
+        println(i)
+    }
+
+
+    //------use of While---------
+    var i = 0
+    // While(i < 15){
+    //     println(i)
+    //     i++;
+    // }
+
+    // do{
+    //     println(i)
+    //     i++
+    // }While(i < 5)
+
+    // var optr = "+";
+    var optr = readLine()?.toLowerCase()
+
+    var operand1 = 23;
+    var operand2 = 32;
+    when(optr){
+        "+" -> println(operand1 + operand2)
+        "-" -> println(operand1 - operand2)
+        "*" -> println(operand1 * operand2)
+        "/" -> println(operand1 / operand2)
+        "%" -> println(operand1 % operand2)
+        // "&&" -> println(operand1 && operand2)
+        // "||" -> println(operand1 || operand2)
+        // "!" -> println(operand1 ! operand2)
+        else -> println("Enter a valid operator")
+    }
+
+    //---------------Operators---------------------
+    println("comparing ")
+    var ac = 30
+    var ad = 40
+    println("ac > ad = "+(ac>ad))
+    println("ac < ad = "+(ac < ad))
+    // println("ac < ad = "+(ac.acompareTo(ad) < 0))
+    println("ac >= ad = "+(ac>=ad))
+    println("ac <= ad = "+(ac <= (ad)))
+    // println("ac <= ad = "+(ac.acompareTo(ad) <= 0))
+    println("ac == ad = "+(ac==ad))
+    println("ac != ad = "+(!(ac?.equals(ad) ?: (ad === null))))
+
+    //----assignment operator------------
+    
+    println("assignment operator ")
+    var aa = 10
+    var ab = 5
+    aa+=ab
+    println(aa)
+    aa-=ab
+    println(aa)
+    aa*=ab
+    println(aa)
+    aa/=ab
+    println(aa)
+    aa%=ab
+    println(aa)
+
+    //---------logical operator------------
+    var ax = 100
+    var ay = 25
+    var az = 10
+    var logicalOprResult = false
+    if(ax > ay && ax > az)
+     println(ax)
+    if(ax < ay || ax > az)
+     println(ay)
+    if( logicalOprResult.not())
+     println("Logical operators")
+
+    //---------------Biteise operator ------
+    println("5 signed shift left by 1 bit: " + 5.shl(1))
+    println("10 signed shift right by 2 bits: : " + 10.shr(2))
+    println("12 unsigned shift right by 2 bits:  " + 12.ushr(2))
+    println("36 bitwise and 22: " + 36.and(22))
+    println("36 bitwise or 22: " + 36.or(22))
+    println("36 bitwise xor 22: " + 36.xor(22))
+    println("14 bitwise inverse is: " + 14.inv())
+
+    //-------------27--- Function------------
+    println("Function")
+    // val str = task()
+    // println(c)
 
 }
+
+// fun task():String {
+   
+//     // return "Performing task"
+//     val str = "Perfroming task"
+//     return str
+// }
+// // }
 //------------------------HOME WORK---------------------------
 // check all the functions which are available with arrays in kotlin 
+// make calculator with the help of When()
+// find factorial using recursion
+// find sum of n numbers using recursion
+// find out the sum of even num for the given range of 1 to n using recursion and funciton calling 
